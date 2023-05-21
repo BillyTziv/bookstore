ad8a709e453a4f73a255ac413946f19960d2b647
Billy Tziv <vtzivaras@gmail.com>
2023-05-21 18:57:19 +0300

    Change Sass to Scss.

--- src/app/layout/item-rating/item-rating.component.ts
+++ src/app/layout/item-rating/item-rating.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
 	selector: 'app-item-rating',
 	templateUrl: './item-rating.component.html',
-	styleUrls: ['./item-rating.component.sass']
+	styleUrls: ['./item-rating.component.scss']
 })
 export class ItemRatingComponent {
 	@Input() rating: number = 0;
